6034c7c8f4ebb7aa6de486e1bb34703ae1c75ae5
saufth <saufth@gmail.com>
2024-02-05 19:02:22 -0600

    Refactor SolucionesPage component and add metadata

--- src/app/(lobby)/soluciones/page.tsx
+++ src/app/(lobby)/soluciones/page.tsx
@@ -1,4 +1,13 @@
-export function SolucionesPage () {
+import { type Metadata } from 'next'
+import { siteConfig } from '@/config/site'
+
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
+  title: 'Soluciones',
+  description: siteConfig.description
+}
+
+export default function SolucionesPage () {
   return (
     <section>
       <h1>En construción..</h1>
